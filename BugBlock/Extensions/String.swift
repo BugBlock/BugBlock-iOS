3d2c5d377b828291dd519fff90b07d22c6f33888
Vadym Kozak <vadimkozak14@gmail.com>
2021-09-06 22:45:36 +0300

    Add metadata

---
+++ BugBlock/Extensions/String.swift
@@ -0,0 +1,8 @@
+//
+//  String.swift
+//  BugBlock
+//
+//  Created by Vadym Kozak on 06.09.2021.
+//
+
+import Foundation
---
+++ BugBlock/Extensions/UIDevice+Device.swift
@@ -0,0 +1,8 @@
+//
+//  UIDevice+Device.swift
+//  BugBlock
+//
+//  Created by Vadym Kozak on 06.09.2021.
+//
+
+import Foundation
